d76548b7207569f7025b5ecf2d71dbb175558099
Maris Veide <maris.veide@gmail.com>
2017-07-19 23:55:46 +0300

    React with Webpacker + Polaris

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,33 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
+
+import React from 'react';
+import {render} from 'react-dom';
+import {Page, Card} from '@shopify/polaris';
+import {EmbeddedApp} from '@shopify/polaris/embedded';
+
+
+class Shopify extends React.Component {
+    render() {
+        return (
+            <Page title="Example application">
+                <Card sectioned>
+                    Insert the rest of your app here, including those components detailed below, which can now
+                    communicate with the Embedded App SDK.
+                </Card>
+            </Page>
+        );
+    }
+}
+
+render(
+    <EmbeddedApp
+        apiKey="a702002fa6ec132f06b951fd231108d3"
+        shopOrigin="https://ultraexportimport.myshopify.com"
+    >
+        <Shopify/>
+    </EmbeddedApp>,
+    document.querySelector('#app') // or another DOM element you want to mount the app in
+);
